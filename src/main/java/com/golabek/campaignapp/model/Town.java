d7972ec911d8159f9a9051e7d5955939a2e78a4e
mglbt <golabek.mat@gmail.com>
2021-03-20 16:33:42 +0100

    prepopulate keyword and town DB

--- src/main/java/com/golabek/campaignapp/model/Town.java
+++ src/main/java/com/golabek/campaignapp/model/Town.java
@@ -20,4 +20,36 @@ public class Town {
     @OneToMany
     @JoinColumn(name = "town_id")
     private Set<Campaign> campaigns;
+
+    public Town() {
+    }
+
+    public Town(@NotNull @NotBlank String name, @NotNull Set<Campaign> campaigns) {
+        this.name = name;
+        this.campaigns = campaigns;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Campaign> getCampaigns() {
+        return campaigns;
+    }
+
+    public void setCampaigns(Set<Campaign> campaigns) {
+        this.campaigns = campaigns;
+    }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO keyword (id, keyword) VALUES (1,'TECHNOLOGY'),(2,'FOOD'),(3,'MEDICAL'),(4,'MUSIC'),(5,'PEOPLE');
+
+INSERT INTO town (id, name) VALUES (1, 'Krakow'),(2, 'Moscow'),(3, 'Canberra'),(4, 'Buenos Aires'),(5, 'Abuja');
