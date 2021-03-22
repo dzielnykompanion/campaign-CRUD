984e32ffe8734c23d699bf7a5c456c4d108e59c0
mglbt <golabek.mat@gmail.com>
2021-03-22 18:56:53 +0100

    models.ts

---
+++ campaign-app-angular-client/src/app/Models.ts
@@ -0,0 +1,28 @@
+export class Seller {
+    id:        number;
+    name:      string;
+    funds:     number;
+    campaigns: Campaign[];
+}
+
+export class Campaign {
+    id:           number;
+    name:         string;
+    keywords:     Keyword[];
+    bidAmount:    number;
+    campaignFund: number;
+    status:       string;
+    town:         Town;
+    radius:       number;
+    sellerId:     number;
+}
+
+export class Keyword {
+    id:      number;
+    keyword: string;
+}
+
+export class Town {
+    id:   number;
+    name: string;
+}
